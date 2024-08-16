83321560d4bf169626ed9b9a1a76eba4d311198a
takeyamakenta <takeyamakenta@gmail.com>
2024-08-16 17:11:43 +0900

    dunno

--- llms/openai/tool.go
+++ llms/openai/tool.go
@@ -2,18 +2,30 @@ package openai
 
 import (
 	"context"
+	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai/internal/openaiclient"
-)
-
-type ToolType = openaiclient.ToolType
-
-const (
-	ToolTypeFunction = openaiclient.ToolTypeFunction
 )
 
 type Tool struct {
 	openaiclient.Tool
 	CallFunc func(ctx context.Context, input string) (string, error)
+}
+
+// NewTool creates a new tool.
+func NewTool(funcDef llms.FunctionDefinition, callFunc func(ctx context.Context, input string) (string, error)) Tool {
+	oaiFuncDef := openaiclient.FunctionDefinition{
+		Name:        funcDef.Name,
+		Description: funcDef.Description,
+		Parameters:  funcDef.Parameters,
+		Strict:      funcDef.Strict,
+	}
+	return Tool{
+		Tool: openaiclient.Tool{
+			Type:		openaiclient.ToolTypeFunction,
+			Function:	oaiFuncDef,
+		},
+		CallFunc: callFunc,
+	}
 }
 
 func (t Tool) Name() string {
